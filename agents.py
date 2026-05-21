102f3d6dada3cb57911faf1cf8748254e51965df
mohammadadil17 <70603116+mohammadadil17@users.noreply.github.com>
2026-05-22 00:16:08 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 #load_dotenv()
 
